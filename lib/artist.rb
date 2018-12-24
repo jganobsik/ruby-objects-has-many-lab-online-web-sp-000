a12b320e07dcd326502479e4f6257238caca3392
Joe Ganobsik <jganobsik@gmail.com>
2018-12-24 02:30:37 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,6 +1,6 @@
 class Artist 
   attr_accessor :name, :songs 
-  
+  @@song_count = []
   def initialize(name)
     @name =  name
     @songs = []
@@ -9,12 +9,14 @@ class Artist
   def add_song(song)
     @songs << song
     song.artist = self
+    @@song_count << song
   end
     
   def add_song_by_name(name)
     new_song = Song.new(name)
     @songs << new_song.title
     new_song.artist =  self
+    @@song_count << song
   end
   
   def Artist.song_count
