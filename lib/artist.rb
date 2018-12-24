73788896e4144812460ebc901623e982c9785f60
Joe Ganobsik <jganobsik@gmail.com>
2018-12-24 02:11:03 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -12,9 +12,9 @@ class Artist
   end
     
   def add_song_by_name(name)
-    name = song.new(name)
+    song.new(name)
     @songs << name
-    @song.artist =  self
+    song.artist =  self
   end
   def song_count
     @songs.length 
