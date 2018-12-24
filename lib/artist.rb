7650766dd866b99d53aa330f9cfbd0dfd64d3a58
Joe Ganobsik <jganobsik@gmail.com>
2018-12-24 02:01:19 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,9 +1,9 @@
 class Artist 
-  attr_accessor :name 
+  attr_accessor :name, :songs 
   
   def initialize(name)
     @name =  name
-    songs = []
+    @songs = []
   end
   
   def add_song(song)
